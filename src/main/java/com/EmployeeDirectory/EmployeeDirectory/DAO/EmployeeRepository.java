38d5f77e6d70f97309402c5bd9896ad9b76b880e
Johnny Wells <john.wells025@gmail.com>
2023-07-14 14:14:54 -0500

    Modified the applitions.properties file to change the base path in Spring Data REST to use '/magic-api' and changed the default page size from 50 to 100. In the EmployeeRepository, I added but later commented it out, the annontation needed to change the url path that Soring Data REST uses to expose and access the endpoints. I temporarily changed it from 'employees' to 'members' to test its functionaily. Once confirmed that it worked, I changed it back to 'employees'.

--- src/main/java/com/EmployeeDirectory/EmployeeDirectory/DAO/EmployeeRepository.java
+++ src/main/java/com/EmployeeDirectory/EmployeeDirectory/DAO/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.EmployeeDirectory.EmployeeDirectory.DAO;
 
 import com.EmployeeDirectory.EmployeeDirectory.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //No need to write any addition code or an implementation class. The JPA Repository has all the methods pre-defined that we need for basic CRUD functionality
 }
