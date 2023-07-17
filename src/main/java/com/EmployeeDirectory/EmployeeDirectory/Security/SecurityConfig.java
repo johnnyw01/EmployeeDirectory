f107e941fe4a999fffc0d9f4b69a42692474772f
Johnny Wells <john.wells025@gmail.com>
2023-07-17 12:52:17 -0500

    Set up Spring security filterChain for authorizing HTTP request based upon roles that hardcoded in the Java program currently and disabled CSRF.

--- src/main/java/com/EmployeeDirectory/EmployeeDirectory/Security/SecurityConfig.java
+++ src/main/java/com/EmployeeDirectory/EmployeeDirectory/Security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.EmployeeDirectory.EmployeeDirectory.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +36,28 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    //Restricting access based on roles
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                );
+
+        //Use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //Disable Cross Site Request Forgery(CSRF)
+        //In general, not required for stateless APIs that use POST,PUT,DELETE and/or PATCH. Usually required for WEB Apps that use HTML forms
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
