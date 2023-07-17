9ea9bc6aafd5353123bb5ed3a60ed8acf62f2863
Johnny Wells <john.wells025@gmail.com>
2023-07-17 13:20:22 -0500

    Updated the application's security to use JDBC to create and store user accounts and information in the datbase instead of hardcoding that information the application's Java code. Created new tables for users and authorities(roles) in the MySQL database and updated the SecurityConfig file to use Spring Security via JDBC to access that information in the database.

--- src/main/java/com/EmployeeDirectory/EmployeeDirectory/Security/SecurityConfig.java
+++ src/main/java/com/EmployeeDirectory/EmployeeDirectory/Security/SecurityConfig.java
@@ -5,37 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-    //In-memory authentication
+    //Add support for JDBC ... no more hardcoding users
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+       return new JdbcUserDetailsManager(dataSource);
     }
 
     //Restricting access based on roles
@@ -60,4 +42,30 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
+//    //In-memory authentication
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 }
